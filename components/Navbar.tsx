0becef662d7dcdbf743f68d42eb96efe17db681c
Chuba-Nwene. C <chinemelumchubanwene57@gmail.com>
2024-04-19 16:24:17 +0200

    add navbar & sidebar for desktop and mobile navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className=' flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-5 lg:px-10'>
+      <Link href="/" className="flex items-center gap-1">
+        <Image 
+          src="/icons/logo.svg"
+          width={35}
+          height={35}
+          alt="callify_icon"
+          className='max-sm:size-10'
+        />
+        <p className="font-extrabold text-white text-[26px] max-sm:hidden">Callify</p>
+      </Link>
+
+      <div className="flex-between gap-5">
+        {/* Clerk - User Management */}
+
+        <MobileNav />
+      </div>
+    </nav>
   )
 }
 
